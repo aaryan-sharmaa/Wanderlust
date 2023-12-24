0eaadd2fa1a3fb60a1126079bf107a8e1ee55909
Aryan Sharma <aryansharma24022004@gmail.com>
2023-12-25 00:31:05 +0530

    add login form

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -21,8 +22,23 @@ router.post(
       req.flash("error", e.message);
       res.redirect("/signup");
     }
-    
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust!");
+    res.redirect("/listings");
+  }
 );
 
 module.exports = router;
