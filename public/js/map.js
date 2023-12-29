0e59a16b0712f87761048c89f327b04b197d33a7
Aryan Sharma <aryansharma24022004@gmail.com>
2023-12-30 00:45:51 +0530

    Add files

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,17 @@
-console.log(mapToken);
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
-  container: "map", // container ID 
+  container: "map",
   style: "mapbox://styles/mapbox/streets-v12",
-  center: [77.216721 , 28.644800], // starting position [lng, lat]
-  zoom: 8, // starting zoom
-}); 
+  center: listing.geometry.coordinates,
+  zoom: 8,
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.title}</h4><p>Exact location will be provided after booking</p>`
+    )
+  )
+  .addTo(map);
